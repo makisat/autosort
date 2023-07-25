46a35d8297b334f60b90f61e591fb703bb601ab5
sato28m <makiya28sato@gmail.com>
2023-07-24 20:10:25 -0400

    simplify code

--- main.go
+++ main.go
@@ -11,18 +11,61 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type fileTypes struct {
+	filetype string
+	directory string
+	extension []string
+}
+
+func (f fileTypes) contains(ext string) bool {
+	for _, e := range f.extension {
+		if e == strings.ToLower(ext) {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("err loading .env:", err)
 	}
 
-	// add download path to .env file
-	// add your own directories here
 	download_directory := os.Getenv("DOWNLOAD_DIRECTORY")
-	video_directory := download_directory + "/videos"
-	image_directory := download_directory + "/images"
-	document_directory := download_directory + "/documents"
+
+	directories := []fileTypes{
+		{
+			filetype: "images",
+			directory: download_directory + "\\images",
+			extension: []string{".jpeg", ".jpg", ".png", ".gif"},
+		},
+		{
+			filetype: "videos",
+			directory: download_directory + "\\videos",
+			extension: []string{".mp4", ".avi", ".mov"},
+		},
+		{
+			filetype: "sounds",
+			directory: download_directory + "\\sounds",
+			extension: []string{".mp3", ".wav", ".ogg"},
+		},
+		{
+			filetype: "documents",
+			directory: download_directory + "\\documents",
+			extension: []string{".pdf", ".docx", ".html", ".txt"},
+		},
+		{
+			filetype: "zip",
+			directory: download_directory + "\\zip",
+			extension: []string{".zip"},
+		},
+		{
+			filetype: "exe",
+			directory: download_directory + "\\exe",
+			extension: []string{".exe"},
+		},
+	}
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -35,59 +78,13 @@ func main() {
 			select {
 			case event := <-watcher.Events:
 				if event.Has(fsnotify.Create) {
-					file := filepath.Base(event.Name)
 					ext := path.Ext(event.Name)
 
-					// add your own extensions here
-					imageExtensions := []string{".jpeg", ".jpg", ".png"}
-					videoExtensions := []string{".mp4", ".avi", ".mov"}
-					documentExtensions := []string{".pdf", ".docx", ".html", ".txt"}
-
-					// todo: create function to check if directory exists
-					// todo: DRY!!!
-					if _, err := os.Stat(video_directory); os.IsNotExist(err) {
-						err := os.Mkdir(video_directory, os.ModePerm)
-						if err != nil {
-							fmt.Println("err at creating directory:", err)
+					for _, directory := range directories {
+						checkDirectory(directory.directory)
+						if directory.contains(ext) {
+							movefile(event.Name, directory.directory)
 						}
-					}
-
-					if _, err := os.Stat(image_directory); os.IsNotExist(err) {
-						err := os.Mkdir(image_directory, os.ModePerm)
-						if err != nil {
-							fmt.Println("err at creating directory:", err)
-						}
-					}
-
-					if _, err := os.Stat(document_directory); os.IsNotExist(err) {
-						err := os.Mkdir(document_directory, os.ModePerm)
-						if err != nil {
-							fmt.Println("err at creating directory:", err)
-						}
-					}
-
-					if contains(strings.ToLower(ext), imageExtensions){
-						err := os.Rename(event.Name, fmt.Sprintf(image_directory+"\\%s", file))
-						if err != nil {
-							fmt.Println("err at moving file:", err)
-						}
-						fmt.Println("move success")
-					}
-
-					if contains(strings.ToLower(ext), videoExtensions){
-						err := os.Rename(event.Name, fmt.Sprintf(video_directory+"\\%s", file))
-						if err != nil {
-							fmt.Println("err at moving file:", err)
-						}
-						fmt.Println("move success")
-					}
-
-					if contains(strings.ToLower(ext), documentExtensions){
-						err := os.Rename(event.Name, fmt.Sprintf(document_directory+"\\%s", file))
-						if err != nil {
-							fmt.Println("err at moving file:", err)
-						}
-						fmt.Println("move success")
 					}
 				}
 			case err := <-watcher.Errors:
@@ -104,11 +101,20 @@ func main() {
 	<-make(chan any)
 }
 
-func contains(value string, list []string) bool {
-	for _, e := range list {
-		if e == value {
-			return true
+func movefile(file string, directory string) {
+	base := filepath.Base(file)
+	err := os.Rename(file, directory + "\\" + base)
+	if err != nil {
+		fmt.Println("err at moving file:", err)
+	}
+}
+
+func checkDirectory(directory string) {
+	if _, err := os.Stat(directory); os.IsNotExist(err) {
+		err := os.Mkdir(directory, os.ModePerm)
+		if err != nil {
+			fmt.Println("err at creating directory:", err)
 		}
 	}
-	return false
 }
+
